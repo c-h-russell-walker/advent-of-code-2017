c66e5b42a1196363fc13fd897ab3c5f4e01e02be
Chris Russell-Walker <c.russell.walker@gmail.com>
2017-12-05 08:17:29 -0500

    CRW Completion of day 4 - part two

--- day4/solution.py
+++ day4/solution.py
@@ -1,22 +1,20 @@
 from argparse import ArgumentParser
 from collections import Counter
 
-def solve_one():
+def solve(second=None):
     valid_count = 0
     with open('./day4/input_1.txt') as input_data:
         for line in input_data.readlines():
             ctr = Counter()
             for word in line.split():
+                if second:
+                    word = ''.join(sorted(word))
                 ctr[word] += 1
             most_common = ctr.most_common(1)[0]
             if most_common[1] == 1:
                 valid_count +=1
 
     print('Number of valid pass phrases: {}'.format(valid_count))
-
-
-def solve_two():
-    pass
 
 
 if __name__ == '__main__':
@@ -28,7 +26,4 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    if not args.second:
-        solve_one()
-    else:
-        solve_two()
+    solve(second=args.second)
